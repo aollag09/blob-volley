8a91ac6b579e2c1df1bc336516d9b0088ce3648c
amaury.ollagnier@gmail.com <amaury.ollagnier@gmail.com@43615252-fc56-e32a-c4a2-e3ffb4505885>
2011-07-06 14:34:31 +0000

    affichage

--- trunk/Blob.java
+++ trunk/Blob.java
@@ -1,3 +1,5 @@
+import java.awt.Graphics;
+
 
 
 /**
@@ -10,18 +12,42 @@
 public class Blob {
 	
 	/* Les constantes */
-	public final static double BLOB_LARGEUR = 0.20;
-	public final static double BLOB_HAUTEUR = 0.30;
+	public final static double BLOB_LARGEUR = 0.10;
+	public final static double BLOB_HAUTEUR = 0.20;
 	
 	/* Le double singleton */
 	public static Blob blobMe = new Blob();
 	public static Blob blobAdversaire = new Blob();
 	
 	/* Les variables d'instance */
-	
+	private double positionX;
+		
 	/* Le constructeur */
 	private Blob(){
+		this.positionX = 1/2;
+	}
+	
+	
+	/** Méthode permettant de tracer le blob dans le graphque passé en paramètre */
+	public void paintBlob(Graphics g){
 		
+		/* On trace dans un premier temps le body */
+		g.fillOval ((int)(Pane.width*positionX),
+				(int)(Pane.height - (int)(Blob.BLOB_HAUTEUR*Pane.height)/2 ),
+				(int)(Blob.BLOB_LARGEUR*(Pane.width+0.0)), 
+				(int)(Blob.BLOB_HAUTEUR*Pane.height));
+	
+		
+	}
+
+
+	public double getPositionX() {
+		return positionX;
+	}
+
+
+	public void setPositionX(double positionX) {
+		this.positionX = positionX;
 	}
 	
 	
--- trunk/Pane.java
+++ trunk/Pane.java
@@ -1,3 +1,4 @@
+import java.awt.Color;
 import java.awt.Graphics;
 
 import javax.swing.JPanel;
@@ -5,7 +6,7 @@ import javax.swing.JPanel;
 
 public class Pane extends JPanel {
 	
-	/* La taille du panel qui peut �tre variable */
+	/* La taille du panel qui peut être variable */
 	public static int width = 500;
 	public static int height = 300;
 	
@@ -18,12 +19,18 @@ public class Pane extends JPanel {
 	
 	
 	public void paintComponent(Graphics g){
+		/* Actualisation des dimensions de la fenêtre */
 		width = getWidth();
 		height = getHeight();
 		
 		
-		g.drawString("caca test",(int) width/3, (int) height/2);
 		
+		g.setColor(Color.RED);
+		Blob.blobMe.paintBlob(g);
+		Blob.blobAdversaire.setPositionX(0.9);
+		g.setColor(Color.BLUE);
+		Blob.blobAdversaire.paintBlob(g);
+	
 		
 	}
 
